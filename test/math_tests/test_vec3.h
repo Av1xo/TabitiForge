c2ab905a9109847d4b4b701ba9eeb7bbc6190502
Av1xo <rostyslav.2004.zhurbenko@gmail.com>
2026-08-16 13:03:55 +0300

    Initial commit

---
+++ test/math_tests/test_vec3.h
@@ -0,0 +1,18 @@
+#ifndef TEST_VEC3_H
+#define TEST_VEC3_H
+
+void test_vec3_add(void);
+void test_vec3_iadd(void);
+void test_vec3_sub(void);
+void test_vec3_isub(void);
+void test_vec3_scale(void);
+void test_vec3_iscale(void);
+void test_vec3_dot(void);
+void test_vec3_cross(void);
+void test_vec3_len(void);
+void test_vec3_len2(void);
+void test_vec3_norm(void);
+void test_vec3_inorm(void);
+
+
+#endif
