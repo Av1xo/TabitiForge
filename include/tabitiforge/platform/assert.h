8b4c67b1955758486b4890ce66dff3050ed88cf7
Av1xo <rostyslav.2004.zhurbenko@gmail.com>
2026-08-24 15:33:53 +0300

    feat(platform): add assert and debug break handling
    - Implement TF_ASSERT and TF_ASSERT_MSG with variadic formatting
    - Integrate TF_DEBUG_BREAK from compiler.h
    - Add TF_STATIC_ASSERT wrapper

---
+++ include/tabitiforge/platform/assert.h
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2026 Rostyslav Zhurbenko, Valeriia Syrotenko
+ * TabitiForge Simulation Engine
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef TABITIFORGE_PLATFORM_ASSERT_H
+#define TABITIFORGE_PLATFORM_ASSERT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "tabitiforge/platform/compiler.h"
+#include "tabitiforge/platform/types.h"
+
+/* =========================================================================
+ * STATIC ASSERT
+ * ========================================================================= */
+
+#define TF_STATIC_ASSERT(cond, msg) _Static_assert(cond, msg)
+
+/* =========================================================================
+ * RUNTIME ASSERTS (DEBUG vs RELEASE)
+ * ========================================================================= */
+#if TF_BUILD == TF_DEBUG
+
+#define TF_ASSERT_HALT(expr_str, file, line, msg_fmt, ...)                                         \
+    do {                                                                                           \
+        fprintf(stderr,                                                                            \
+                "\n[TABITIFORGE ASSERT FAILED]\n"                                                  \
+                "  Expression : %s\n"                                                              \
+                "  File       : %s:%d\n"                                                           \
+                "  Message    : " msg_fmt "\n\n",                                                  \
+                expr_str, file, line, ##__VA_ARGS__);                                              \
+        fflush(stderr);                                                                            \
+        TF_DEBUG_BREAK();                                                                          \
+    } while (0)
+
+#define TF_ASSERT_MSG(condition, msg_fmt, ...)                                                     \
+    do {                                                                                           \
+        if (!(condition)) {                                                                        \
+            TF_ASSERT_HALT(#condition, __FILE__, __LINE__, msg_fmt, ##__VA_ARGS__);                \
+        }                                                                                          \
+    } while (0)
+
+#define TF_ASSERT(condition) TF_ASSERT_MSG(condition, "No details provided")
+
+#else
+
+#define TF_ASSERT(condition)               ((void)0)
+#define TF_ASSERT_MSG(condition, msg, ...) ((void)0)
+
+#endif
+
+#endif // TABITIFORGE_PLATFORM_ASSERT_H
